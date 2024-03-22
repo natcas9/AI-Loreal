f49df9819aab3ee578879430c60eaba17522c4b9
Natalia Casarin <natalia.casarin02@gmail.com>
2024-03-22 01:30:58 -0600

    fixed problems

--- src/app.js
+++ src/app.js
@@ -12,15 +12,23 @@ function generateAI(response) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7cfac3t6d90fo31f4fb0ff0151e8560c";
   let context =
-    "You are an expert chef who loves to Christmas. Your mission is to plan a Christmas recipe in basic HTML. Make sure to be precise and detailed with bullet points and show the recipe complete. Make sure to follow the instructions below and give the complete instructions.Make the h1 a little bit smaller. Please! at the end of the recipe and at the  write a Christmas quote and sign with SheCodes AI in bold, ";
-  let prompt = `User instructions: Generate a Christmas recipe of ${instructionsInput.value}`;
+    " You are a professional stylist helper who loves to be innovative and cretaive with your work.Your mission is to create a set of recommendation of Loreal products and hair styles writing it like an html list. Make sure to be precise and detailed with bullet points and show the recommendation complete. Make sure to follow the instructions below and give the complete instructions. Make the h1 a little bit smaller.Please give the information in bullet points. Please! at the end of the recommendation and at the  write a quote and sign with SheCodes AI in bold.";
+
+  let prompt = `User instructions: Generate hair recommendations using Loreal products  ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(showResponse);
   let recipe = document.querySelector("#reponse-generator");
   recipe.classList.remove("hidden");
-  recipe.innerHTML = `<div class="generating">Generating your Christmas recipe of ${instructionsInput.value}  🎄❤️ !</div>`;
+  recipe.innerHTML = `<div class="generating">Generating the best recommendations for your client: ${instructionsInput.value} !</div>`;
   instructionsInput.value = "";
+}
+
+function generateReport() {
+  alert("Report generated and emailed successfully!");
 }
 
 let inputGenerator = document.querySelector("#form-generator");
 inputGenerator.addEventListener("submit", generateAI);
+
+let report = document.querySelector("#report");
+report.addEventListener("click", generateReport);
