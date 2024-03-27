884ad5e380fb90f5d26ee22259e115d28a6d5cce
Natalia Casarin <natalia.casarin02@gmail.com>
2024-03-27 12:12:05 -0600

    fixed style

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ function generateAI(response) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7cfac3t6d90fo31f4fb0ff0151e8560c";
   let context =
-    " You are a professional stylist helper who loves to be innovative and cretaive with your work.Your mission is to create a set of recommendation of Loreal products and hair styles writing it like an html list. Make sure to be precise and detailed with bullet points and show the recommendation complete. Make sure to follow the instructions below and give the complete instructions. Make the h1 a little bit smaller.Please give the information in bullet points. Please! at the end of the recommendation and at the  write a quote and sign with SheCodes AI in bold.";
+    " You are a professional stylist helper who loves to be innovative and cretaive with your work.Your mission is to create a set of recommendation of Loreal products and hair styles writing it like an html list. Make sure to be precise and detailed with bullet points and show the recommendation complete. Make sure to follow the instructions below and give the complete instructions. Make the h1 a little bit smaller.Please give the information in bullet points. Please! at the end of the recommendation  sign with SheCodes AI in bold.";
 
   let prompt = `User instructions: Generate hair recommendations using Loreal products  ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(showResponse);
   let recipe = document.querySelector("#reponse-generator");
   recipe.classList.remove("hidden");
-  recipe.innerHTML = `<div class="generating">Generating the best recommendations for your client: ${instructionsInput.value} !</div>`;
+  recipe.innerHTML = `<div class="generating">Generating the best recommendations for our client: ${instructionsInput.value} !</div>`;
   instructionsInput.value = "";
 }
 
@@ -28,6 +28,7 @@ function generateReport() {
 }
 
 let inputGenerator = document.querySelector("#form-generator");
+
 inputGenerator.addEventListener("submit", generateAI);
 
 let report = document.querySelector("#report");
